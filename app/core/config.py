9950e09e8e53306aa5a371b0d668a6be637d4c97
Pragy Agarwal <agar.pragy@gmail.com>
2025-12-22 18:14:25 +0530

    Explain the default values in config

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """
+    Note: All values defined here are defaults and will be overridden automatically
+    by environment variables thanks to pydantic-settings.
+
+    For example, the docker-compose.yml file sets environment variables to the following values
+        REDIS_NODES=redis://redis-node-1:6379,redis://redis-node-2:6379
+        DEBUG=true
+    """
+
     # Redis Configuration
     REDIS_NODES: str = ""  # Comma-separated list of redis URLs
 
